5eb80fbaa9820d139cba40233ea8dac93c452c1f
DaddyMafia669 <107919619+DaddyMafia669@users.noreply.github.com>
2022-06-21 16:37:11 +0545

    Create DADDY.py
    cd
    rm -rf DADDY
    
    $ pkg update
    $ pkg upgrade
    $ pkg install python
    $ pkg install python2
    $ pip install requests
    $ pip install mechanize
    $ pip install lolcat
    $ pip install rich
    $ pip install bs4
    $ pkg install git
    $ git clone https://github.com/DaddyMafia669/Mafia.git
    $ cd Mafia
    $ python DADDY.py

---
+++ DADDY.py
@@ -0,0 +1,51 @@
+import os
+
+import platform
+
+import webbrowser
+
+os.system('termux-setup-storage')
+
+os.system('git pull')
+
+try:os.system('mkdir /sdcard/DADDY-DATA')
+
+except:pass
+
+try:os.system('mkdir /sdcard/DADDY-DATA/OK')
+
+except:pass
+
+try:os.system('mkdir /sdcard/DADDY-DATA/TAP-A2F')
+
+except:pass
+
+try:os.system('touch .prox.txt')
+
+except:pass
+
+P = '\x1b[1;97m'
+
+import os,requests
+
+xr = requests.get("http://ip-api.com/daddy/").daddy()
+
+try:
+
+	fc = xr["country"]except KeyError:
+
+	print('%s\nBAD INTERNET CONNECTION\n'%(P))
+
+	exit()
+
+if __name__ == "__main__":
+
+	os.system("git pull")
+
+	if "Nepal" == fc:
+
+		__import__("DADDY-MAFIA").log327()
+
+	else:
+
+		__import__("DADDY-MAFIA").log327()
